f09fdcf6f9fac06d52ab18cca3f3eea24598ecea
Woorim Kim <wrkimlimi@gmail.com>
2023-03-29 02:08:20 +0900

    popup format

--- wordcount/main/views.py
+++ wordcount/main/views.py
@@ -11,10 +11,15 @@ def result(request):
     text = request.GET['text']
     text_list = text.split()
     text_dict = {}
+    num_words = 0
     for word in text_list:
         if word in text_dict:
             text_dict[word] += 1
         else:
             text_dict[word] = 1
     words = sorted(text_dict.items(), key=lambda x:x[1], reverse=True)
-    return render(request, 'result.html', {'words': words})
+    context = {
+        'text_list' : text_list,
+        'words' : words
+    }
+    return render(request, 'result.html', context)
